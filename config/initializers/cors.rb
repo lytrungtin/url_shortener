32da81ad69626265ba9a19f3191a2e008e509c64
Tin Ly <trungtin.sotech@gmail.com>
2022-12-29 04:25:02 +0700

    auto correct rubocop

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,17 +1,19 @@
+# frozen_string_literal: true
+
 # Be sure to restart your server when you modify this file.
 
 # Avoid CORS issues when API is called from the frontend app.
 # Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin AJAX requests.
 
 # Read more: https://github.com/cyu/rack-cors
-if ['development', 'production'].include? ENV['RAILS_ENV']
+if %w[development production].include? ENV['RAILS_ENV']
   Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins ENV["ALLOWED_ORIGINS"]
-  
-      resource "*",
-        headers: :any,
-        methods: [:post]
+      origins ENV['ALLOWED_ORIGINS']
+
+      resource '*',
+               headers: :any,
+               methods: [:post]
     end
-  end  
+  end
 end
