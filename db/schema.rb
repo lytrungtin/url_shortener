37025bcbe295133e070e7a9af14e7159d9c2364b
Tin Ly <trungtin.sotech@gmail.com>
2023-08-07 13:30:54 +0700

    update build fe

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,15 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_221_227_171_400) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_27_171400) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'urls', force: :cascade do |t|
-    t.string 'original_url'
-    t.string 'slug'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['slug'], name: 'index_urls_on_slug', unique: true
+  create_table "urls", force: :cascade do |t|
+    t.string "original_url"
+    t.string "slug"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["slug"], name: "index_urls_on_slug", unique: true
   end
+
 end
